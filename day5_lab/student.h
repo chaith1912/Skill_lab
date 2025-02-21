5b4dd68f0ec6cf9db4fadad9229d34c89f01d8f6
Chaithanya R Rao <raochaithanya56@gmail.com>
2025-02-21 15:31:58 +0530

    c programming

---
+++ day5_lab/student.h
@@ -0,0 +1,13 @@
+
+#ifndef STUDENT_HEADER
+#define STUDENT_HEADER
+struct Student_t{
+    int rollNo;
+    char name[50];
+    int maths, physics, chemistry, cs;
+    int totalMarks;
+    float percentage;
+    char grade;
+};
+typedef struct Student_t Student;
+#endif
